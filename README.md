74f3084f2671398ff88e96deb59e8e444463a9f3
thomas <thomas@tpoe.dev>
2024-08-05 11:15:56 +0200

    refactoring(*): add info page and add bmh ids

--- README.md
+++ README.md
@@ -1,30 +1,15 @@
-# React + TypeScript + Vite
+# Marker
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Software to find, classify and mark tree microhabitats for [Förderprogramm klimaangepasstes Waldmanagement](https://www.bmel.de/DE/themen/wald/klimaangepasstes-waldmanagement.html)
 
-Currently, two official plugins are available:
+## Description
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Usage
 
-## Expanding the ESLint configuration
+The application can be used under the following domain https://marker.tpoe.dev or self hosted with docker.
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+## Resources:
 
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+- https://www.bmel.de/DE/themen/wald/klimaangepasstes-waldmanagement.html
+- https://www.klimaanpassung-wald.de/
+- https://www.waldwissen.net/de/lebensraum-wald/naturschutz/habitatbaeume-kennen-schuetzen-und-foerdern
--- src/statistics/statistics.tsx
+++ src/statistics/statistics.tsx
@@ -1,144 +1,128 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react';
 
-import './statistics.css'
+import './statistics.css';
 
-import { Point, loadData, loadLayers, pointInPolygon } from '../utils'
+import { BMH } from '../bmh';
+import { Point, loadData, loadLayers, pointInPolygon } from '../utils';
 
 function Statistics() {
-    const [polygons, setPolygons] = useState<
-        Array<{
-            id: string
-            name: string
-            markers: number
-            bmhs: Map<string, number>
-        }>
-    >([])
-    const [selectedPolygonIds, setSelectedPolygonIds] = useState<Array<string>>(
-        [],
-    )
+  const [polygons, setPolygons] = useState<
+    Array<{
+      id: string;
+      name: string;
+      markers: number;
+      bmhs: Map<number, number>;
+    }>
+  >([]);
+  const [selectedPolygonIds, setSelectedPolygonIds] = useState<Array<string>>([]);
 
-    useEffect(() => {
-        const layers = loadLayers()
-        const data = loadData()
+  useEffect(() => {
+    const layers = loadLayers();
+    const data = loadData();
 
-        const polygonLayer = layers?.features.filter(
-            (m: any) => m.geometry.type === 'Polygon',
-        )
+    const polygonLayer = layers?.features.filter((m: any) => m.geometry.type === 'Polygon');
 
-        const markers = layers?.features.filter(
-            (m: any) => m.geometry.type === 'Point',
-        )
+    const markers = layers?.features.filter((m: any) => m.geometry.type === 'Point');
 
-        const preparedPolygons = polygonLayer.map((layer: any) => {
-            const id = layer.properties.featureId
-            const d = data.find((d) => d.id === id)
+    const preparedPolygons = polygonLayer.map((layer: any) => {
+      const id = layer.properties.featureId;
+      const d = data.find((d) => d.id === id);
 
-            const area = layer.geometry.coordinates[0].map(
-                ([lat, lng]: [number, number]) => {
-                    return new Point(lat, lng)
-                },
-            )
+      const area = layer.geometry.coordinates[0].map(([lat, lng]: [number, number]) => {
+        return new Point(lat, lng);
+      });
 
-            const bmhs = new Map<string, number>()
-            let internalCounter = 0
-            markers.forEach((marker: any) => {
-                const id = marker.properties.featureId
-                const dataPoint = data.find((d) => d.id === id)
+      const bmhs = new Map<number, number>();
+      let internalCounter = 0;
+      markers.forEach((marker: any) => {
+        const id = marker.properties.featureId;
+        const dataPoint = data.find((d) => d.id === id);
 
-                const point = new Point(
-                    marker.geometry.coordinates[0],
-                    marker.geometry.coordinates[1],
-                )
+        const point = new Point(marker.geometry.coordinates[0], marker.geometry.coordinates[1]);
 
-                if (pointInPolygon(point, area)) {
-                    internalCounter += 1
+        if (pointInPolygon(point, area)) {
+          internalCounter += 1;
 
-                    dataPoint?.bmh?.map((v: string) => {
-                        const value = bmhs.get(v)
-                        if (value) {
-                            bmhs.set(v, value + 1)
-                        } else {
-                            bmhs.set(v, 1)
-                        }
-                    })
-                }
-            })
-
-            return {
-                id: id,
-                name: d?.name ?? 'Unnamed',
-                markers: internalCounter,
-                bmhs: bmhs ?? [],
-            }
-        })
-
-        setPolygons(preparedPolygons)
-        setSelectedPolygonIds(preparedPolygons.map((p: { id: string }) => p.id))
-    }, [])
-
-    function handleCheckbox(id: string): void {
-        setSelectedPolygonIds((prev) => {
-            if (prev.includes(id)) {
-                return prev.filter((p) => p !== id)
+          dataPoint?.bmh?.map((v: number) => {
+            const value = bmhs.get(v);
+            if (value) {
+              bmhs.set(v, value + 1);
             } else {
-                return [...prev, id]
+              bmhs.set(v, 1);
             }
-        })
-    }
+          });
+        }
+      });
 
-    const totalMarkers = useMemo(() => {
-        return polygons
-            .filter((p) => selectedPolygonIds.includes(p.id))
-            .reduce((acc, val) => acc + val.markers, 0)
-    }, [polygons, selectedPolygonIds])
+      return {
+        id: id,
+        name: d?.name ?? 'Unnamed',
+        markers: internalCounter,
+        bmhs: bmhs ?? [],
+      };
+    });
 
-    const totalBMHs = useMemo(() => {
-        return polygons
-            .filter((p) => selectedPolygonIds.includes(p.id))
-            .reduce((acc, val) => {
-                val.bmhs.forEach((value, key) => {
-                    const currentValue = acc.get(key)
-                    if (currentValue) {
-                        acc.set(key, currentValue + value)
-                    } else {
-                        acc.set(key, value)
-                    }
-                })
+    setPolygons(preparedPolygons);
+    setSelectedPolygonIds(preparedPolygons.map((p: { id: string }) => p.id));
+  }, []);
 
-                return acc
-            }, new Map<string, number>())
-    }, [polygons, selectedPolygonIds])
+  function handleCheckbox(id: string): void {
+    setSelectedPolygonIds((prev) => {
+      if (prev.includes(id)) {
+        return prev.filter((p) => p !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
+  }
 
-    return (
-        <div className="statistics">
-            <h2>Polygons</h2>
-            <ul className="polygons">
-                {polygons.map((p) => (
-                    <li key={p.id} onClick={() => handleCheckbox(p.id)}>
-                        <input
-                            defaultChecked={true}
-                            checked={selectedPolygonIds.includes(p.id)}
-                            type="checkbox"
-                        />
-                        <span>{p.name}</span>
-                    </li>
-                ))}
-            </ul>
-            <h2>General</h2>
-            <span>Total Markers: {totalMarkers}</span>
-            <h2>Statistics</h2>
-            <ul>
-                {Array.from(totalBMHs)
-                    .sort((a, b) => b[1] - a[1])
-                    .map(([key, value]) => (
-                        <li key={key}>
-                            <span>{key}:</span>
-                            <span>{value}</span>
-                        </li>
-                    ))}
-            </ul>
-        </div>
-    )
+  const totalMarkers = useMemo(() => {
+    return polygons.filter((p) => selectedPolygonIds.includes(p.id)).reduce((acc, val) => acc + val.markers, 0);
+  }, [polygons, selectedPolygonIds]);
+
+  const totalBMHs = useMemo(() => {
+    return polygons
+      .filter((p) => selectedPolygonIds.includes(p.id))
+      .reduce((acc, val) => {
+        val.bmhs.forEach((value, key) => {
+          const currentValue = acc.get(key);
+          if (currentValue) {
+            acc.set(key, currentValue + value);
+          } else {
+            acc.set(key, value);
+          }
+        });
+
+        return acc;
+      }, new Map<number, number>());
+  }, [polygons, selectedPolygonIds]);
+
+  return (
+    <div className="statistics">
+      <h2>Polygons</h2>
+      <ul className="polygons">
+        {polygons.map((p) => (
+          <li key={p.id} onClick={() => handleCheckbox(p.id)}>
+            <input defaultChecked={true} checked={selectedPolygonIds.includes(p.id)} type="checkbox" />
+            <span>{p.name}</span>
+          </li>
+        ))}
+      </ul>
+      <h2>General</h2>
+      <span>Total Markers: {totalMarkers}</span>
+      <h2>Statistics</h2>
+      <ul>
+        {Array.from(totalBMHs)
+          .sort((a, b) => b[1] - a[1])
+          .map(([key, value]) => (
+            <li key={key}>
+              <span>{BMH.find((v) => v.id === key)?.name}:</span>
+              <span>{value}</span>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Statistics
+export default Statistics;
